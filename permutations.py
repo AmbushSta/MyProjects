5b1c0ba10efce6f51d19665af1b7f32d56af1aec
AmbushSta <michael2437@hotmail.com>
2016-12-14 19:30:16 +1300

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,15 @@
+"""Given a list it will return the maximum permutations of the given list"""
+
+def perms(s,index = 0, sLength = None,answer = set()):
+    if sLength == None:
+        sLength = len(s)
+    if index == sLength:
+        answer.add(tuple(s))    
+    s_List = list(s)
+    for i in range(index,sLength):
+        s_List[index],s_List[i] = s_List[i],s_List[index]
+        perms(s_List,index + 1,sLength,answer) 
+        s_List[index],s_List[i] = s_List[i],s_List[index]
+    return answer
+
+print(sorted(perms([5,6,7,10,11])))
