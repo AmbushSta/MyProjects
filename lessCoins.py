20c55ab5f5f91dc6253f5dc822a2a6da2327048d
AmbushSta <michael2437@hotmail.com>
2016-12-14 16:34:54 +1300

    Update lessCoins.py

--- lessCoins.py
+++ lessCoins.py
@@ -1,3 +1,7 @@
+"""Takes in an integer representing the amount of cents you have, and prints
+the amount of notes you can acquire, with the fifty cents and ten cents
+being the remaining"""
+
 def lessCoins(n_cents):
     nzCurrency = [(500, 'Five dollars'),(200,'Two dollars'),(100, 'One dollar'),
                   (50, 'Fifty cents'),(10, 'Ten cents')]
