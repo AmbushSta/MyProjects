e01b23bb7f43a1d3591f9a6d2cd96738c1d2cb39
AmbushSta <michael2437@hotmail.com>
2016-12-14 19:00:13 +1300

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,1 +1,28 @@
-"""Given a list as input, gives all possible combinations that add up to 21"""
+"""Given a list returns all possible combinations of numbers that add to
+21, prints into shell"""
+
+def twentyone(seq):
+    answer = []
+    current_path = []
+    
+    for index, seq_number in enumerate(seq):
+        for subSetIndex in range(index + 1,len(seq)):
+            current_path = [seq_number]
+            for added_num in seq[subSetIndex:]:
+                current_path.append(added_num)
+                if(sum(current_path)) == 21:
+                    answer.append(current_path)
+                    break
+                elif (sum(current_path)) > 21:
+                    break
+                
+    if len(answer) > 0:
+        for anAnswer in answer:
+            print("The values ", end = "")
+            for value in anAnswer:
+                print("{} ".format(value), end = "")
+            print("add up to 21")
+
+#Example
+seq = [5, 6, 7,10,11,7,3]
+twentyone(seq)
