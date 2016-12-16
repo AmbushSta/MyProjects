34079341fa7b7d091a8c2d1468cc73a620a4dedc
AmbushSta <michael2437@hotmail.com>
2016-12-17 08:10:08 +1300

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -6,10 +6,10 @@ def perms(s, index = 0, sLength = None, answer = set()):
     if index == sLength:
         answer.add(tuple(s))    
     s_List = list(s)
-    for i in range(index,sLength):
-        s_List[index],s_List[i] = s_List[i],s_List[index]
-        perms(s_List,index + 1,sLength,answer) 
-        s_List[index],s_List[i] = s_List[i],s_List[index]
+    for i in range(index, sLength):
+        s_List[index], s_List[i] = s_List[i], s_List[index]
+        perms(s_List, index + 1, sLength,answer) 
+        s_List[index] , s_List[i] = s_List[i], s_List[index]
     return answer
 
 print(sorted(perms([5,6,7,10,11])))
