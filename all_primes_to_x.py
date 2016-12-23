bec9a5d012bf16f86e87903a6a1a00f23948e5c5
AmbushSta <michael2437@hotmail.com>
2016-12-23 15:47:43 +1300

    Update all_primes_to_x.py

--- all_primes_to_x.py
+++ all_primes_to_x.py
@@ -1,1 +1,29 @@
+'''
+Prime generator, made to practice and learn generators and the yield keyword
+'''
 
+
+import math
+
+def all_primes(upper_limit):
+    if upper_limit < 3:
+        return
+    current_number = 3
+    for a_prime in get_primes(current_number):
+        if a_prime > upper_limit:
+            return
+        print(a_prime)
+        
+def get_primes(current_number):
+    while True:
+        if is_prime(current_number):
+            yield current_number
+        current_number += 1
+        
+def is_prime(number):
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(number)) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
