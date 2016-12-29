67bb00eb9f8435cf76c9129cfb29ae412892f1c5
AmbushSta <michael2437@hotmail.com>
2016-12-30 00:47:39 +1300

    Update lessCoins.py

--- lessCoins.py
+++ lessCoins.py
@@ -5,19 +5,26 @@ Author: Michael Cowie
 """
 
 def lessCoins(n_cents):
-    nzCurrency = [(500, 'Five dollars'),(200,'Two dollars'),(100, 'One dollar'),
-                  (50, 'Fifty cents'),(10, 'Ten cents')]
+    nzCurrency = [(500, 'Five dollar note'),(200,'Two dollar coin'),(100, 'One dollar coin'),
+                  (50, 'Fifty cent coin'),(10, 'Ten cent coin')]
     for value, currency in nzCurrency:
         requiredAmount = n_cents // value
         if requiredAmount > 0:
-            print("You will require {} {} pieces".format(requiredAmount, currency))
+            if currency.split()[2] == "coin" and requiredAmount != 1:
+                print("You will require {} {}s".format(requiredAmount, currency))
+            elif currency.split()[2] == "coin" and requiredAmount == 1:
+                print("You will require {} {}".format(requiredAmount, currency))
+            elif currency.split()[2] == "note" and requiredAmount != 1:
+                print("You will require {} {}s".format(requiredAmount, currency))
+            else:
+                print("You will require {} {}".format(requiredAmount, currency))                 
             n_cents = n_cents - (requiredAmount * value)
             
 lessCoins(12345)
 """
-Outputs:
-You will require 24 Five dollars pieces
-You will require 1 Two dollars pieces
-You will require 1 One dollar pieces
-You will require 4 Ten cents pieces
+Will output::
+You will require 24 Five dollar notes
+You will require 1 Two dollar coin
+You will require 1 One dollar coin
+You will require 4 Ten cent coins
 """
