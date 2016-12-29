b0bccfc3623cea5af0d5174e0abf88bf96ebaef3
AmbushSta <michael2437@hotmail.com>
2016-12-29 19:34:17 +1300

    Update 2DArrays.py

--- 2DArrays.py
+++ 2DArrays.py
@@ -1,12 +1,10 @@
 '''Creates a 2DArray from input variables'''
+
 x = int(input())
 y = int(input())
 column, row = x, y
 matrix = [[0 for x in range(column)] for y in range(row)]
 print(matrix)
-
-
-
 
 '''
 Note:
