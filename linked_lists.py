8da622912cde4453fb9adbc258459aa1180ddb46
AmbushSta <michael2437@hotmail.com>
2016-12-29 22:15:15 +1300

    Update linked_lists.py

--- linked_lists.py
+++ linked_lists.py
@@ -12,7 +12,7 @@ class Node:
         self.data = data
         
         
-class LinkedList:
+class Linkedlist:
     def __init__(self, root = None):
         self.root = None
         self.size = 0
@@ -26,22 +26,28 @@ class LinkedList:
         previous_node = None
         current_node = self.root
         while current_node != None:
-            current_node_data = current_node.get_data()
-            if current_node_data == data and previous_node != None:
-                previous_node.set_next(current_node.get_next())
-                size -= 1
-                return True
-            #Case when deleting the root
-            elif current_node_data == data and previous_node == None:
-                self.root = current_node.get_next()
-                size -= 1
+            if current_node.get_data() == data:
+                if previous_node != None:
+                    previous_node.set_next(current_node.get_next())
+                else:
+                    self.root = None
+                self.size -= 1
                 return True
             else:
-                return False
+                current_node = current_node.get_next()
+        return False
+    def display_data(self):
+        current_node = self.root
+        node_number = 0
+        while current_node != None:
+            print("Node number {} has the data {}".format(node_number, current_node.get_data()))
+            node_number += 1
+            current_node = current_node.get_next()
+            
                 
 #TestCases
-first_Node = Node(5)
-second_Node = Node(10, first_Node)
-third_Node = Node(15, second_Node)
-fourth_Node = Node(20, third_Node)
-fifth_Node = Node(25, fourth_Node)
+myList = Linkedlist()
+myList.insert_node(20)
+myList.insert_node(15)
+myList.insert_node(10)
+myList.insert_node(5)
