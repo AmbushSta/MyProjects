bffb2b19e400e228de12318b5a63f4856a062ecb
AmbushSta <michael2437@hotmail.com>
2016-12-29 16:44:35 +1300

    Update 2DArrays.py

--- 2DArrays.py
+++ 2DArrays.py
@@ -4,3 +4,12 @@ y = int(input())
 column, row = x, y
 matrix = [[0 for x in range(column)] for y in range(row)]
 print(matrix)
+
+
+
+
+'''
+Note:
+2D matricies are created by column, and then row but are indexed
+matrix[x][y] row by column
+'''
