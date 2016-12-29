77bdf23397e51fe20b643bc3062a531fc35ec422
AmbushSta <michael2437@hotmail.com>
2016-12-29 18:40:24 +1300

    Update 21_using_recursion.py

--- 21_using_recursion.py
+++ 21_using_recursion.py
@@ -1,1 +1,12 @@
+def twentyone(nums, stack = [], answer = set()):
+    for index, num in enumerate(nums):
+        new_stack = stack + [num]
+        total = sum(new_stack)
+        if total == 21:
+            answer.add(tuple(new_stack))
+        elif total < 21:
+            twentyone(nums[index + 1:], new_stack, answer)
+    return answer
 
+answer = twentyone([1,9,11,5,6])
+print(answer)
