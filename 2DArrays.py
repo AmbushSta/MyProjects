0f30bb9c2019b2634786e1836f7b6ea1c3603e1a
AmbushSta <michael2437@hotmail.com>
2016-12-29 19:35:39 +1300

    Update 2DArrays.py

--- 2DArrays.py
+++ 2DArrays.py
@@ -1,13 +1,12 @@
 '''Creates a 2DArray from input variables'''
 
-x = int(input())
-y = int(input())
-column, row = x, y
+column = int(input())
+row = int(input())
 matrix = [[0 for x in range(column)] for y in range(row)]
 print(matrix)
 
 '''
-Note:
+Notes:
 2D matricies are created by column, and then row but are indexed
 matrix[x][y] row by column
 '''
