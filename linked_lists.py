fdd9fa8e45173063ae3757ce62139b2da2516db5
AmbushSta <michael2437@hotmail.com>
2016-12-29 21:04:43 +1300

    Update linked_lists.py

--- linked_lists.py
+++ linked_lists.py
@@ -1,1 +1,9 @@
 
+class Node:
+    def __init__(self, data = None, next_node = None):
+        self.data = data
+        self.next_node = next_node
+    def set_next(self, node_object):
+        self.next_node = node_object
+    def set_data(self, data):
+        self.data = data
