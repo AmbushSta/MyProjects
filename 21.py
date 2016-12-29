f8592df9ccbbbaf21cae51571f65479e3d2ae64b
AmbushSta <michael2437@hotmail.com>
2016-12-30 00:28:11 +1300

    Update 21.py

--- 21.py
+++ 21.py
@@ -6,7 +6,6 @@ Author: Michael Cowie
 
 def twentyone(seq):
     answer = []
-    
     for index, seq_number in enumerate(seq):
         for subset_index in range(index + 1, len(seq)):
             current_path = [seq_number]
@@ -17,23 +16,25 @@ def twentyone(seq):
                     break
                 elif sum(current_path) > 21:
                     break
+    return answer
+                              
+user_input = input()
+list_format = [int(x) for x in user_input.split()]
+answer = twentyone(list_format)
                 
-    if len(answer) > 0:
-        for an_answer in answer:
-            print("The values ", end = "")
-            for a_combination in an_answer:
-                print("{} ".format(a_combination), end = "")
-            print("add up to 21")
-
-#Example
-seq = [5, 6, 7,10,11,7,3]
-twentyone(seq)
+if len(answer) == 0:
+    print("No combination of numbers add to 21")
+for solution in answer:
+    print("The values ", end = "")
+    for number in solution:
+        print("{} ".format(number), end = "")
+    print("add up to 21")
 
 
 """
+Using Example numbers "5, 6, 7,10,11,7,3"
 Output from example:
 The values 10 11 add up to 21
 The values 11 7 3 add up to 21
-
-Fail case [1,9,11,5,6], corrected using recursion method
+Fail case [1,9,11,5,6] (missing 1 + 9 + 5 + 6), corrected using recursion method
 """
