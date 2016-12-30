e07c845a20b01999f4b3dbf4b3281938c6d766bb
AmbushSta <michael2437@hotmail.com>
2016-12-30 21:40:04 +1300

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -26,13 +26,13 @@ answer = perms(list_format)
 if len(answer) == 0:
     print("No possible permutations")
 else:
-    print("The following sequence are all permutations from the input \"{}\"".format(" ".join(str(x) for x in list_format)))
+    print("The following combinations are all permutations from the input \"{}\"".format(" ".join(str(x) for x in list_format)))
     for solution in answer:
         print("{}".format(" ".join(str(x) for x in solution)))
 """"
 Example:
 Input "5 6 7" should output:
-The following sequence are all permutations from the input "5 6 7"
+The following combinations are all permutations from the input "5 6 7"
 5 7 6
 5 6 7
 6 5 7
