27910329a6707ff6a4082ceaf75491deb2357133
AmbushSta <michael2437@hotmail.com>
2017-01-04 15:12:43 +1300

    Update project_euler_Q169(Power of Two).py

--- project_euler_Q169(Power of Two).py
+++ project_euler_Q169(Power of Two).py
@@ -9,16 +9,10 @@ For example f(10) = 5 since there are five ways to express 10:
 2 + 8
 what is (n) for a given n
 '''
+import math
+
 x = int(input())
-
-def highest_power(x):
-    current_highest = 0
-    while 2 ** current_highest <= x:
-        current_highest += 1
-    return current_highest - 1
-
-current_highest = highest_power(x)
-
+current_highest = math.floor(math.log(x,2))
 recursion_list = []
 
 for power in range(current_highest + 1):
@@ -28,10 +22,10 @@ for power in range(current_highest + 1):
 def euler169(user_input, current_stack = [], answer = set()):
     for index, num in enumerate(user_input):
         new_stack = current_stack + [num]
-        if sum(new_stack) == 10:
+        if sum(new_stack) == x:
             answer.add(tuple(new_stack))
             return
-        elif sum(new_stack) < 10:
+        elif sum(new_stack) < x:
             euler169(user_input[index +1:], new_stack, answer)
     return answer
 
