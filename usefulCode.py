eb3e7a5017f6d487f5a5e2da1d467c88b28df7bd
Michael Cowie <mco116@uclive.ac.nz>
2017-06-28 15:47:48 +1200

    Update usefulCode.py

--- usefulCode.py
+++ usefulCode.py
@@ -2,7 +2,7 @@
 Contains small, but very useful code.
 """
 
-#Find the most frequent letter in a string using the max() function
+#Find the most frequent occruence of words in a single string seperated by a space.
 test_string = "x x y  "
 most_common = max(test_string, key = lambda x : test_string.split(" ").count(x))
 print(most_common)  #1
