7caf6f1eed3118be8e1e43b44410cc9ad9bb7497
Michael Cowie <mco116@uclive.ac.nz>
2017-06-28 16:16:58 +1200

    Update useful_tricks.py

--- useful_tricks.py
+++ useful_tricks.py
@@ -7,18 +7,22 @@ test_string = "x x y z Michael-Cowie"
 most_common = max(test_string, key = lambda x : test_string.split(" ").count(x))
 print(most_common)  #1
 
+
 #Tuple sorting by 2nd index.
 tuple_list = [(10,9),(8,7),(6,5),(4,3),(2,1)]
 print(sorted(tuple_list, key = lambda x : x[1]))  #[(2, 1), (4, 3), (6, 5), (8, 7), (10, 9)]
 
+
 #Fill up someones ram ;)
 x = [n for n in range(-2**62, 2**62)]
+
 
 #Doubley nested dictionaries, made easy.
 depth = 5
 nested_dict = {x : dict() for x in range(depth)}
 nested_dict[0][1] = 5 #Example
 print(nested_dict[0][1]) #5
+
 
 #How to represent a map, in the form of a x,y plane.
 map_str = """\
