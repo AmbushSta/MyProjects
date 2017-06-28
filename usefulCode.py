2605c0c239f114e15e7de354b0d414b80213f990
Michael Cowie <mco116@uclive.ac.nz>
2017-06-28 15:51:52 +1200

    Update usefulCode.py

--- usefulCode.py
+++ usefulCode.py
@@ -10,3 +10,6 @@ print(most_common)  #1
 #Tuple sorting by 2nd index
 tuple_list = [(10,9),(8,7),(6,5),(4,3),(2,1)]
 print(sorted(tuple_list, key = lambda x : x[1]))  #[(2, 1), (4, 3), (6, 5), (8, 7), (10, 9)]
+
+#Fill up someones ram ;)
+x = [x for x in range(-2**62, 2**62)]
