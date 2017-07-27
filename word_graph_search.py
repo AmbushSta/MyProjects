b564904b7d0c59edc9885a7e2bd252d73129e018
Michael Cowie <mco116@uclive.ac.nz>
2017-07-27 22:53:12 +1200

    Update word_graph_search.py

--- word_graph_search.py
+++ word_graph_search.py
@@ -13,22 +13,24 @@ directions = [('N' , -1, 0), ('NE', -1, 1),
 
 def count_Win_Occurence(user_map):
     occurences = 0
-    twoDArray = user_map.split("\n")
-    for row in range(len(twoDArray)):
-        for col in range(len(twoDArray[row])):
-            if twoDArray[row][col] == "W":
-                occurences += count_Occurences_From(row, col, twoDArray)
+    twoD_Array = user_map.split("\n")
+    for row in range(len(twoD_Array)):
+        for col in range(len(twoD_Array[row])):
+            if twoD_Array[row][col] == "W":
+                occurences += count_Occurences_From(row, col, twoD_Array)
     return occurences
 
-def count_Occurences_From(row, col, user_map, goal_string = "IN"):
+def count_Occurences_From(row, col, user_map, goal_string = "IN", direction_list = []):
     if goal_string == "":
+        print("Possible solution : " + " ".join(direction_list))
         return 1
     occurences = 0
-    for _, row_add, col_add in directions:
+    for direction, row_add, col_add in directions:
         new_row = row + row_add
         new_col = col + col_add        
         if user_map[new_row][new_col] == goal_string[0]:
-            occurences += count_Occurences_From(new_row, new_col, user_map, goal_string[1:])
+            direction_stack = [direction] + direction_list
+            occurences += count_Occurences_From(new_row, new_col, user_map, goal_string[1:], direction_stack)
     return occurences
 
 my_map = """
