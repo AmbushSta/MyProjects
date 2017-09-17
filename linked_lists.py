636bfdfa021b59fa05b8a3fa7847ac656334b3b2
Michael Cowie <mco116@uclive.ac.nz>
2017-09-17 12:15:38 +1200

    Update linked_lists.py

--- linked_lists.py
+++ linked_lists.py
@@ -8,9 +8,6 @@ class Node:
         self.data = data
         self.next_node = next_node
         
-    def get_next(self):
-        return self.next_node
-    
     def set_next(self, node_object):
         self.next_node = node_object
         
