c7569204e17c8373a1fb13e7b75acf724a6327fd
Michael Cowie <mco116@uclive.ac.nz>
2017-09-18 22:54:46 +1200

    Update matrixMultiplication.py

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -1,31 +1,25 @@
 """
-    Multiplies two matricies, else returns 0.
     Self Notes:
         Two matrix a x b and c x d will only be valid if b = c. The resulting
         matrix will have dimensions a x d.
     Author: Michael Cowie
 """
 
-def multiply_two_matricies(A, B):
-    
-    try:
-        if len(A[0]) != len(B):
-            raise Exception("Mate you can't multiply these")
-    except IndexError:
+def multiply_matricies(A, B):
+    if len(A[0]) != len(B):
         raise Exception("Mate you can't multiply these")
     
-    answer = [
+    return [
         [sum(x * y for x, y in zip(col, row)) for col in zip(*B)] for row in A
     ]
     
-    return answer
-    
     
 
-matrix1 = []
+matrix1 = [[1,2],
+           [3,4]]
 
 matrix2 = [[1,2],
            [3,4]]
 
-result = multiply_two_matricies(matrix1, matrix2)
+result = multiply_matricies(matrix1, matrix2)
 print(result) #[[7, 10], [15, 22]]
