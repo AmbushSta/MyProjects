f33949cbe1a87e37bc9bf4aac81536fd72e95ea5
Michael Cowie <mco116@uclive.ac.nz>
2018-05-09 14:58:11 +1200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -10,8 +10,10 @@ hw = 'Hello World'
 while i < len(hw):
     letter = random.randint(ord(' '), ord('z') + 1)
     new_string = current + chr(letter)
-    print(new_string, end='\r')
-    time.sleep(0.01)
+    
     if chr(letter) == hw[i]:
         current += chr(letter)
         i += 1
+        
+    print(new_string, end='\r')
+    time.sleep(0.01)
