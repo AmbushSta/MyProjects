fcb80ee502dda92813d5c6e21d2102ccde400386
Michael Cowie <mco116@uclive.ac.nz>
2018-09-01 23:06:28 +1200

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -3,13 +3,19 @@ class Vehicle():
     def __init__(self):
         self.is_a_vehicle = True
         self.print_myself = {
-            Car : car_print,
-            Bike : bike_print   
+            Car : self.car_print,
+            Bike : self.bike_print   
         }
 
     def __str__(self):
         return self.print_myself[type(self)]()
+    
+    def car_print(self):
+        return "I am a car!"    
         
+    def bike_print(self):
+        return "I am a bike!"
+    
 class Bike(Vehicle):
     
     def __init__(self):
@@ -21,12 +27,6 @@ class Car(Vehicle):
     def __init__(self):
         super().__init__()
         self.wheel_count = 4
-        
-def car_print():
-    return "I am a car!"
-    
-def bike_print():
-    return "I am a bike!"
         
 car = Car()
 print(car) # I am a car!
