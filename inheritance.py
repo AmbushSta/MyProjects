759bbc763df853a6ac2871924db3bebdfb4a51e5
Michael Cowie <mco116@uclive.ac.nz>
2018-09-01 23:18:32 +1200

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,14 +1,14 @@
 class Vehicle():
     
     def __init__(self):
-        self.is_a_vehicle = True
+        self.is_a_vehicle = True # Object variable for all Vehicles
         self.print_myself = {    # Map each object to the correct method
             Car : self.car_print,
             Bike : self.bike_print   
         }
 
     def __str__(self):
-        return self.print_myself[type(self)]()  # Reduce if statements by mapping
+        return self.print_myself[type(self)]()  # Reduce if statement chain by mapping
     
     def car_print(self):
         return "I am a car!"    
@@ -20,7 +20,7 @@ class Bike(Vehicle):
     
     def __init__(self):
         super().__init__()     # Calling parent __init__ method
-        self.wheel_count = 2   
+        self.wheel_count = 2   # Object variable for only Bike objects
         
 class Car(Vehicle):
     
